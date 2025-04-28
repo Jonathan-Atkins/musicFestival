989a503547e4251246e14d567c9f09ac4dc1cb45
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-28 12:13:47 -0700

    feat: first passing model spec for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :schedules
   has_many :shows, through: :schedules
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, presence: true
 end
