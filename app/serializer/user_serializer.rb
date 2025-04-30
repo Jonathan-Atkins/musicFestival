543ae5b711d025dde3675fcba2e62c30b725d0b3
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-30 16:56:04 -0600

    feat: updating UserSerializer

--- app/serializer/user_serializer.rb
+++ app/serializer/user_serializer.rb
@@ -2,4 +2,8 @@ class UserSerializer
   include JSONAPI::Serializer
 
   attributes :first_name, :last_name, :email, :username, :birthday
+
+  attribute :schedule_id do |user|
+    user.schedule&.id
+  end
 end
