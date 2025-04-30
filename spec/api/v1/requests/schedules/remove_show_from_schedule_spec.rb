e20759a578f4fd5ffe09dbb2847e9834e91bc28c
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-30 00:51:39 -0600

    feat: udpate edge casing for remove show from schedule

--- spec/api/v1/requests/schedules/remove_show_from_schedule_spec.rb
+++ spec/api/v1/requests/schedules/remove_show_from_schedule_spec.rb
@@ -27,22 +27,32 @@ RSpec.describe 'Remove Show from Schedule', type: :request do
     context 'sad path' do
       it 'returns 404 if the schedule does not exist' do
         delete "/api/v1/users/#{@user.id}/schedules/0/shows/#{@shows.first.id}"
-      
+    
         expect(response.status).to eq(404)
         
         body = JSON.parse(response.body, symbolize_names: true)
-        
         expect(body[:errors].first[:detail]).to eq("Schedule not found")
       end
-      
+    
       it 'returns 404 if the show does not exist in the schedule' do
         invalid_show_id = 999
         delete "/api/v1/users/#{@user.id}/schedules/#{@schedule.id}/shows/#{invalid_show_id}"
-      
+    
         expect(response.status).to eq(404)
         
         body = JSON.parse(response.body, symbolize_names: true)
+        expect(body[:errors].first[:detail]).to eq("Show not found in the schedule")
+      end
+    
+      it 'returns 404 if the user tries to delete a show from an empty schedule' do
+        @schedule.shows.clear
+        @schedule.reload
+    
+        delete "/api/v1/users/#{@user.id}/schedules/#{@schedule.id}/shows/#{@shows.first.id}"
+    
+        expect(response.status).to eq(404)
         
+        body = JSON.parse(response.body, symbolize_names: true)
         expect(body[:errors].first[:detail]).to eq("Show not found in the schedule")
       end
     end
