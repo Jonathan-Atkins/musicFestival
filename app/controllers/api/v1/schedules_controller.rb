9919b2a131bc1b5bb3fec21056e8a870084e4501
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-04-29 18:17:53 -0700

    feat:updated get schedule for user

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,16 +1,16 @@
 # app/controllers/api/v1/schedules_controller.rb
 class Api::V1::SchedulesController < ApplicationController
   def show
-    user = User.find(params[:user_id])
-    schedule = user.schedule
-
-    if schedule
-      render json: ScheduleSerializer.new(schedule)
+    @user = User.find(params[:user_id])
+    @schedule = @user.schedule
+  
+    if @schedule
+      render json: ScheduleSerializer.new(@schedule)
     else
       render json: { errors: [{ detail: "Schedule not found" }] }, status: :not_found
     end
-
-  rescue ActiveRecord::RecordNotFound => e
+  
+  rescue ActiveRecord::RecordNotFound
     render json: { errors: [{ detail: "User not found" }] }, status: :not_found
   end
 end
