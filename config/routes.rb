30d4234654ad6b4fade5e0be8300e62957b10ab9
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-05-01 08:34:53 -0600

    small update

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :index, :show] do
-        get :find, on: :collection  # 👈 this adds /api/v1/users/find
+        get :find, on: :collection  
         resources :schedules, only: [:index, :show, :create] do
           resources :shows, only: [:create, :destroy, :index]
         end
