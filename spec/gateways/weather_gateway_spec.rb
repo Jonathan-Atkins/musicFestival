f8fe02ae00ba67b0ceb2d9b00012ef5bdacc4e6e
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-06-04 12:12:52 -0700

    updated WeatherGateway Spec

--- spec/gateways/weather_gateway_spec.rb
+++ spec/gateways/weather_gateway_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe WeatherGateway, :vcr do
-  it 'can successfully call unsplash database' do
+  it 'fetches weather data for a given ZIP code' do
     zip_code = '89032'
     weather  = WeatherGateway.fetch_weather(zip_code)
+
+    expect(weather).to be_a(Weather)
+    expect(weather.city).to eq('North Las Vegas')
+    expect(weather.description).to be_a(String)
+    expect(weather.temp_min).to be_a(Numeric)
+    expect(weather.temp_max).to be_a(Numeric)
   end
 end
