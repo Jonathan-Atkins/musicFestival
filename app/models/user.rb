55cd2e698f30fd7055e89fbb765a25481eb005a4
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-08-27 20:53:41 -0700

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
   has_one :schedule, dependent: :destroy
+  has_many :shows, through: :schedule
   validates :first_name, presence: true
   validates :last_name,  presence: true
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
             format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username,   presence: true
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false }
   validates :birthday,   presence: true
+  
+  before_validation { self.email = email.to_s.downcase.strip }
 
   after_create_commit :create_default_schedule
 
